8530e1cdfed79984e4b4d45a04f4dbfb9dab4349
Wagsong Djomegni <m.wagsong.djomegni@accenture.com>
2025-09-02 11:50:44 +0400

    feat():projet de base

---
+++ revision-concept/src/main/java/com/example/concept/revision/RevisionConceptApplication.java
@@ -0,0 +1,13 @@
+package com.example.concept.revision;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevisionConceptApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevisionConceptApplication.class, args);
+	}
+
+}
---
+++ revision-concept/src/test/java/com/example/concept/revision/RevisionConceptApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.concept.revision;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RevisionConceptApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
